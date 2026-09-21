714462742c979e3dc58784fca7d94321b23e9941
junwoo-qkr <p40212@gmail.com>
2026-09-21 14:57:06 +0900

    기술 태그 기능 기본 구조 추가

---
+++ src/main/java/com/mini8/backend/features/tech/ctrl/TechController.java
@@ -0,0 +1,22 @@
+package com.mini8.backend.features.tech.ctrl;
+
+import com.mini8.backend.features.tech.service.TechService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tech-tags")
+@RequiredArgsConstructor
+public class TechController {
+
+  private final TechService techService;
+
+  // getTechList: 기술 칩 목록 불러오기
+  @GetMapping("/")
+  public ResponseEntity<?> getTechList() {
+    return null;
+  }
+}
---
+++ src/main/java/com/mini8/backend/features/tech/domain/entity/TechEntity.java
@@ -0,0 +1,16 @@
+package com.mini8.backend.features.tech.domain.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Builder
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class TechEntity {}
