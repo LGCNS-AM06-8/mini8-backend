664b813703e62fc1df2897fad383474c4da5519b
4404 <joeynoh2002@naver.com>
2026-09-22 10:21:57 +0900

    bookmark pk수정

--- src/main/java/com/mini8/backend/database/bookmark/domain/entity/BookmarkEntity.java
+++ src/main/java/com/mini8/backend/database/bookmark/domain/entity/BookmarkEntity.java
@@ -3,6 +3,9 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 
@@ -18,6 +21,8 @@ import jakarta.persistence.UniqueConstraint;
     }
 )
 public class BookmarkEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bookmark_id;
     private Long user_id;
     private Long blog_post_id;
