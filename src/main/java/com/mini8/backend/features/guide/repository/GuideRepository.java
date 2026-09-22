e3dd523000982ea6c5248ec4ee03287762b5eae3
junwoo-qkr <p40212@gmail.com>
2026-09-22 09:25:37 +0900

    refactor(guide)!: 미사용 가이드 entity와 repository 제거

--- src/main/java/com/mini8/backend/features/guide/repository/GuideRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mini8.backend.features.guide.repository;
-
-import com.mini8.backend.features.guide.domain.entity.GuideEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GuideRepository extends JpaRepository<GuideEntity, Void> {}
