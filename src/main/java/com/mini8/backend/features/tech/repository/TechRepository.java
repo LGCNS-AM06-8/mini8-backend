31c80b37f448040df3ca18905049b252a2e2d6f0
junwoo-qkr <p40212@gmail.com>
2026-09-22 09:31:08 +0900

    refactor(tech)!: 미사용 기술 태그 entity와 repository 제거

--- src/main/java/com/mini8/backend/features/tech/repository/TechRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mini8.backend.features.tech.repository;
-
-import com.mini8.backend.features.tech.domain.entity.TechEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TechRepository extends JpaRepository<TechEntity, Void> {}
