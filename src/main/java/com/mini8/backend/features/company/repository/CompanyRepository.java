a925e3b722e2a722464ea45c17e10e78dc1aa03b
junwoo-qkr <p40212@gmail.com>
2026-09-22 09:31:50 +0900

    refactor(company)!: 미사용 기업 entity와 repository 제거

--- src/main/java/com/mini8/backend/features/company/repository/CompanyRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mini8.backend.features.company.repository;
-
-import com.mini8.backend.features.company.domain.entity.CompanyEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<CompanyEntity, Void> {}
