3831caf419f22dbd97368223733f6f6a0bccc4db
junwoo-qkr <p40212@gmail.com>
2026-09-22 09:52:48 +0900

    feat(bookmark): 북마크 기능 기본 구조 및 Swagger 명세 추가
    * feat(bookmark): 북마크 기능 기본 구조 추가
    
    * docs(bookmark): 북마크 API Swagger 명세 추가
    
    * refactor(bookmark)!: 미사용 북마크 entity와 repository 제거

---
+++ src/main/java/com/mini8/backend/features/bookmark/service/BookmarkService.java
@@ -0,0 +1,22 @@
+package com.mini8.backend.features.bookmark.service;
+
+import com.mini8.backend.features.company.domain.dto.CompanyResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookmarkService {
+
+  public CompanyResponseDTO addBookmark(int userId) {
+    return null;
+  }
+
+  public CompanyResponseDTO getBookmark() {
+    return null;
+  }
+
+  public CompanyResponseDTO deleteBookmark() {
+    return null;
+  }
+}
