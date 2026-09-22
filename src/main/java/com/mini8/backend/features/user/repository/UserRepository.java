2d0f1d83487923b183f1d6aef9f519d32f812e76
junwoo-qkr <p40212@gmail.com>
2026-09-22 09:28:30 +0900

    refactor(user)!: 미사용 사용자 entity와 repository 제거

--- src/main/java/com/mini8/backend/features/user/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mini8.backend.features.user.repository;
-
-import com.mini8.backend.features.user.domain.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Void> {}
