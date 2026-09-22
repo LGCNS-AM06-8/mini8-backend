c9c780e23baed49a1e3f5a20906b2fa671bc6ad6
junwoo-qkr <p40212@gmail.com>
2026-09-22 09:29:21 +0900

    refactor(bookmark)!: 미사용 북마크 entity와 repository 제거

--- src/main/java/com/mini8/backend/features/bookmark/repository/BookmarkRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mini8.backend.features.bookmark.repository;
-
-import com.mini8.backend.features.bookmark.domain.entity.BookmarkEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookmarkRepository extends JpaRepository<BookmarkEntity, Void> {}
