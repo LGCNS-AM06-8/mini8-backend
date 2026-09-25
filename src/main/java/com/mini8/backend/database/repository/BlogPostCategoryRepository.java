49830d5029eae8f1b46d7b0360d712bfca730e6e
4404 <joeynoh2002@naver.com>
2026-09-25 22:57:56 +0900

    fix: add bookmark용 레포 수정

--- src/main/java/com/mini8/backend/database/repository/BlogPostCategoryRepository.java
+++ src/main/java/com/mini8/backend/database/repository/BlogPostCategoryRepository.java
@@ -2,7 +2,22 @@ package com.mini8.backend.database.repository;
 
 import com.mini8.backend.database.blog.domain.entity.BlogPostCategoryEntity;
 import com.mini8.backend.database.blog.domain.entity.BlogPostCategoryPk;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BlogPostCategoryRepository
-    extends JpaRepository<BlogPostCategoryEntity, BlogPostCategoryPk> {}
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+    public interface BlogPostCategoryRepository
+        extends JpaRepository<BlogPostCategoryEntity, BlogPostCategoryPk> {
+
+            @Query("""
+        SELECT c
+        FROM BlogPostCategoryEntity c
+        WHERE c.blogPost.blog_post_id = :postId
+    """)
+    List<BlogPostCategoryEntity> findCategoriesByPostId(
+            @Param("postId") Long postId
+    );
+    }
