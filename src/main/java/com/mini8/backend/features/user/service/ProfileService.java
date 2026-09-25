7c81f3dff06e17e074216a800c8579107e996520
junwoo-qkr <p40212@gmail.com>
2026-09-25 14:32:03 +0900

    feat(user): 프로필 조회 API 구현

--- src/main/java/com/mini8/backend/features/user/service/ProfileService.java
+++ src/main/java/com/mini8/backend/features/user/service/ProfileService.java
@@ -1,7 +1,16 @@
 package com.mini8.backend.features.user.service;
 
+import com.mini8.backend.commons.exception.BusinessException;
+import com.mini8.backend.commons.exception.ErrorCode;
+import com.mini8.backend.database.User.domain.entity.UserEntity;
+import com.mini8.backend.database.User.domain.entity.UserJobFieldEntity;
+import com.mini8.backend.database.User.domain.entity.UserSkillEntity;
+import com.mini8.backend.database.repository.UserJobFieldRepository;
 import com.mini8.backend.database.repository.UserRepository;
+import com.mini8.backend.database.repository.UserSkillRepository;
 import com.mini8.backend.features.user.domain.dto.ProfileResponseDTO;
+import com.mini8.backend.features.user.domain.dto.ProfileResponseDTO.SkillResponseDTO;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,44 +20,44 @@ public class ProfileService {
 
   private final UserRepository userRepository;
 
-  // private final UserJobFieldRepository userJobFieldRepository;
-  // private final UserSkillRepository userSkillRepository;
+  private final UserJobFieldRepository userJobFieldRepository;
+  private final UserSkillRepository userSkillRepository;
 
   public ProfileResponseDTO setProfile() {
     return null;
   }
 
-  // TODO: UserJobFieldRepository와 UserSkillRepository 추가
-  // TODO: 추가 후에는 임시로 넣어둔 return null; 을 지우고 주석 처리된 코드 주석 해제
   public ProfileResponseDTO getProfile(Long userId) {
-    return null;
-    // UserEntity user = userRepository.findById(userId)
-    //   .orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND));
+    UserEntity user =
+        userRepository
+            .findById(userId)
+            .orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND));
 
-    // List<String> jobFields = userJobFieldRepository.findAllByUser(user).stream()
-    //   .map(UserJobFieldEntity::getJobField)
-    //   .toList();
+    List<String> jobFields =
+        userJobFieldRepository.findAllByUser(user).stream()
+            .map(UserJobFieldEntity::getJobField)
+            .toList();
 
-    // List<SkillResponseDTO> haveSkills = userSkillRepository.findAllByUserAndSkillType(user,
-    // "HAVE").stream()
-    //   .map(UserSkillEntity::getTechTag)
-    //   .map(techTag -> new SkillResponseDTO(techTag.getTech_tag_id(), techTag.getName()))
-    //   .toList();
-    // List<SkillResponseDTO> wantSkills = userSkillRepository.findAllByUserAndSkillType(user,
-    // "WANT").stream()
-    //   .map(UserSkillEntity::getTechTag)
-    //   .map(techTag -> new SkillResponseDTO(techTag.getTech_tag_id(), techTag.getName()))
-    //   .toList();
+    List<SkillResponseDTO> haveSkills =
+        userSkillRepository.findAllByUserAndSkillType(user, "HAVE").stream()
+            .map(UserSkillEntity::getTechTag)
+            .map(techTag -> new SkillResponseDTO(techTag.getTech_tag_id(), techTag.getName()))
+            .toList();
+    List<SkillResponseDTO> wantSkills =
+        userSkillRepository.findAllByUserAndSkillType(user, "WANT").stream()
+            .map(UserSkillEntity::getTechTag)
+            .map(techTag -> new SkillResponseDTO(techTag.getTech_tag_id(), techTag.getName()))
+            .toList();
 
-    // return ProfileResponseDTO.builder()
-    //   .userId(user.getUser_id())
-    //   .name(user.getName())
-    //   .jobFields(jobFields)
-    //   .careerYears(user.getCareer_years())
-    //   .haveSkills(haveSkills)
-    //   .wantSkills(wantSkills)
-    //   .profileVersion(user.getProfile_version())
-    //   .build();
+    return ProfileResponseDTO.builder()
+        .userId(user.getUser_id())
+        .name(user.getName())
+        .jobFields(jobFields)
+        .careerYears(user.getCareer_years())
+        .haveSkills(haveSkills)
+        .wantSkills(wantSkills)
+        .profileVersion(user.getProfile_version())
+        .build();
   }
 
   public ProfileResponseDTO editProfile() {
